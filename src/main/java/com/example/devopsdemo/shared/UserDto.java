d9e6cd4d9db9e3a147dbfb4d24ee7d451314461d
Youssef Faik <faik.yusef@gmail.com>
2023-03-07 12:46:53 +0100

    add UserDto

---
+++ src/main/java/com/example/devopsdemo/shared/UserDto.java
@@ -0,0 +1,17 @@
+package com.example.devopsdemo.shared;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    private long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String encryptedPassword;
+
+  
+}
